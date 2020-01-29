94f14922d165df35a43f6127e6a37474ac78f801
Gavcor74 <38769589+Gavcor74@users.noreply.github.com>
2020-01-29 23:40:04 +0100

    actualización formularios

---
+++ 18-formularios/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formularios PHP y HTML</title>
+</head>
+<body>
+    <h1 align="center">Formulario</h1>
+        <form action="" method="" enctype="multipart/form-data">
+
+
+
+
+
+        </form>
+
+
+
+
+<?php
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
